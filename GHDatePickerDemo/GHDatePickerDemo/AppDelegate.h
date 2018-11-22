50e4aa8d495c3d49b9f7a71995881012fcbc7ed8
yin tian <yintian@macbook-pro-2.local>
2018-11-22 14:01:11 +0800

    init 项目

---
+++ GHDatePickerDemo/GHDatePickerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GHDatePickerDemo
+//
+//  Created by yin tian on 2018/11/22.
+//  Copyright © 2018 yin tian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GHDatePickerDemo/GHDatePickerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GHDatePickerDemo
+//
+//  Created by yin tian on 2018/11/22.
+//  Copyright © 2018 yin tian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
