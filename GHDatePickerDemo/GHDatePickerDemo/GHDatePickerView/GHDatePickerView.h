ac5a989e428fd6c7d6b136d4fa1ab8e71b38bab1
yin tian <yintian@macbook-pro-2.local>
2018-11-22 14:37:21 +0800

    为方法添加注释说明

--- GHDatePickerDemo/GHDatePickerDemo/GHDatePickerView/GHDatePickerView.h
+++ GHDatePickerDemo/GHDatePickerDemo/GHDatePickerView/GHDatePickerView.h
@@ -23,14 +23,25 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface GHDatePickerView : BRBaseView
 
-
-
+/**
+ *  1.显示时间选择器
+ *
+ *  @param title            标题
+ *  @param defaultValue     默认值
+ *  @param type         日期显示类型
+ *  @param currentDate  默认选中的时间（值为空/值格式错误时，默认就选中现在的时间）
+ *  @param mindate      设置日期滚动上限
+ *  @param maxDate      设置日期滚动下限
+ *  @param resultBlock      选择结果的回调
+ *
+ */
 + (void)showPickerViewWithTitle:(NSString *)title defaultValue:(NSString *)defaultValue dataType:(GHDatePickerMode)type currentDate:(NSDate *)currentDate minDate:(NSDate *)mindate maxDate:(NSDate *)maxDate resultBlock:(GHDateResultBlock)resultBlock;
+
 //清理缓存
 - (void)cleanCache;
-//判断文件是否已经在沙盒中已经存在？
+ 
+//判断数据是否已经在沙盒中已经存在？
 -(BOOL) isFileExist:(NSString *)fileName;
-
 
 @end
 
