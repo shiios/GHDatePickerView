91672a8920fe527f594a55cd4ab5c8f5ad52a25c
yin tian <yintian@macbook-pro-2.local>
2018-11-22 14:24:16 +0800

    定制化日期格式显示，弥补系统及第三方支持不同模式

--- GHDatePickerDemo/GHDatePickerDemo/ViewController.m
+++ GHDatePickerDemo/GHDatePickerDemo/ViewController.m
@@ -8,6 +8,8 @@
 
 #import "ViewController.h"
 
+
+
 @interface ViewController ()
 
 @end
@@ -17,6 +19,16 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    NSDate *minDate = [NSDate br_setYear:2017 month:1 day:1];
+    NSDate *maxDate = [NSDate br_setYear:2037 month:1 day:1];
+    [GHDatePickerView showPickerViewWithTitle:@"休假" defaultValue:@"" dataType:GHDatePickerModeDate currentDate:[NSDate date] minDate:minDate maxDate:maxDate resultBlock:^(NSString *selectDateStr, NSString *ampmStr) {
+        
+        NSLog(@"%@%@",selectDateStr,ampmStr);
+    }];
 }
 
 
