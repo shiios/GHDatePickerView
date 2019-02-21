8d21ea20cce14bc015079c3dfe53ef7cec22830d
sheen <sf03040504@gmail.com>
2019-02-21 14:41:48 +0800

    配置podspec

---
+++ GHCustomAlertViewController/GHCustomAlertViewController/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  GHCustomAlertViewController
+//
+//  Created by yin tian on 2018/11/22.
+//  Copyright © 2018 yin tian. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
---
+++ GHCustomAlertViewController/GHCustomAlertViewController/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GHCustomAlertViewController
+//
+//  Created by yin tian on 2018/11/22.
+//  Copyright © 2018 yin tian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
